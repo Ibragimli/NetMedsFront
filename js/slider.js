abfe0a17361d184e214ed76206541da3b8aa028e
Ibragimli <elnur204@gmail.com>
2022-02-11 15:56:32 +0400

    slider

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        nav: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+
+        }
+    })
+
+
+})
