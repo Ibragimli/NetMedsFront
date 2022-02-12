0f1f87fd02f217eda823d9f86c50d71048eb1d45
Ibragimli <elnur204@gmail.com>
2022-02-12 20:30:57 +0400

    basket+ login+ register+ labtest-+

---
+++ js/basketOwl.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        nav: false,
+        responsive: {
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
---
+++ js/mostOwl.js
@@ -0,0 +1,17 @@
+$('.owl-carousel-mostOwl').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1250:{
+            items:3
+        }
+    }
+})
