b47a0f8fe842e4e8928c47397e19a2963e9fd00e
Ibragimli <elnur204@gmail.com>
2022-02-22 00:33:32 +0400

    aboutUs update shopfilter fixed productdetail fixed

--- js/labTests.js
+++ js/labTests.js
@@ -1,14 +1,15 @@
 var today = new Date();
 var dd = today.getDate();
-console.log(dd)
-
-var mm = today.getMonth() + 1;
+var mm = today.getMonth() + 1; //January is 0!
 var yyyy = today.getFullYear();
 
-var maxmm = today.getMonth() + 2;
+if (dd < 10) {
+  dd = '0' + dd;
+}
 
-$('#dateTime').ejDateTimePicker({
-    width: 200,
-    minDateTime: dd + "/" + mm + "/" + yyyy,
+if (mm < 10) {
+  mm = '0' + mm;
+}
 
-});
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("datefield").setAttribute("min", today);
